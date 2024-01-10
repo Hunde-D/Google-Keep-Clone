c62705fc8d2b0ae2613f2e9b08366e10aebcb0a0
Hunde-D <hunddh2025@gmail.com>
2024-01-10 11:15:06 +0300

    update app.js expanding and closing Notes

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 class App {
     constructor() {
-        console.log('app works!')
+        console.log('app works!');
+        this.$form = document.querySelector('#form');
+        this.$noteTitle = document.querySelector('#note-title');
+        this.$formButtons = document.querySelector('#form-buttons');
+        
+        this.addEventListeners();
+    }
+    addEventListeners() {
+        document.body.addEventListener('click', event =>{
+            this.handleClick(event);
+            
+            
+        })
+    }
+    handleClick(event){
+        const isFormClicked = this.$form.contains(event.target);
+        
+        if(isFormClicked) {
+            this.openForm();
+        }else{
+            this.closeForm();
+            
+        
+        }
+    }
+    openForm(){
+        console.log('form open works!');
+        this.$form.classList.add('form-open');
+        this.$noteTitle.style.display = 'block';
+        this.$formButtons.style.display = 'block';
+    }
+    closeForm() {
+        console.log('form close works!');
+        this.$form.classList.remove('form-open');
+        this.$noteTitle.style.display = 'none';
+        this.$formButtons.style.display = 'none';
     }
 }
 
