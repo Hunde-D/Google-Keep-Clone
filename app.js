dd4267d7c3727001b607ba0c15fb574baf6c6390
Hunde-D <hunddh2025@gmail.com>
2024-01-10 21:29:35 +0300

    added edit functionality

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ class App {
     constructor() {
         console.log('app works!');
         this.notes =[];
+        this.editTitle = '';
+        this.editText = '';
+        this.editId = '';
         
         this.$form = document.querySelector('#form');
         this.$note = document.querySelector('#notes');
@@ -9,12 +12,20 @@ class App {
         this.$noteTitle = document.querySelector('#note-title');
         this.$noteText = document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
+        this.$formCloseButton = document.querySelector('#form-close-button');
+        this.$modal = document.querySelector(".modal");
+        this.$modalTitle = document.querySelector(".modal-title");
+        this.$modalText = document.querySelector(".modal-text");
+        this.$modalCloseButton = document.querySelector(".modal-close-button");
+
 
         this.addEventListeners();
     }
     addEventListeners() {
         document.body.addEventListener('click', event =>{
             this.handleClick(event);
+            this.selectNote(event);
+            this.openModal(event);
         })
         this.$form.addEventListener('submit', event => {
            event.preventDefault(); 
@@ -26,16 +37,28 @@ class App {
                   this.addNote({title, text});
               }
         });
+        this.$formCloseButton.addEventListener('click', event => {
+            event.stopPropagation();
+            this.closeForm();
+        })
+        this.$modalCloseButton.addEventListener('click', event =>{
+            this.closeModal(event);
+        })
     }
     handleClick(event){
         const isFormClicked = this.$form.contains(event.target);
-        
+
+        const title = this.$noteTitle.value;
+        const text = this.$noteText.value;
+        const hasNote = title || text;
+
         if(isFormClicked) {
             this.openForm();
+        }else if(hasNote){
+            this.addNote({title, text});
         }else{
             this.closeForm();
-            
-        
+
         }
     }
     openForm(){
@@ -44,28 +67,56 @@ class App {
         this.$formButtons.style.display = 'block';
     }
     closeForm() {
+        this.$noteTitle.value = '';
+        this.$noteText.value = '';
         this.$form.classList.remove('form-open');
         this.$noteTitle.style.display = 'none';
         this.$formButtons.style.display = 'none';
-        this.$noteTitle.value = '';
-        this.$noteText.value = '';
     }
-    addNote(note){
+    openModal(event){
+        if (event.target.closest('.note')) {
+            this.$modal.classList.toggle('open-modal');
+            this.$modalTitle.value = this.editTitle;
+            this.$modalText.value = this.editText;
+        }
+
+    }
+    closeModal(event) {
+        this.editNote();
+        this.$modal.classList.toggle('open-modal');
+    }
+    addNote({title,text}){
         const newNote ={
-            title: note.title,
-            text: note.text,
+            title,
+            text,
             color: 'white',
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
         };
         this.notes = [...this.notes, newNote];
-        this.diplayNotes();
+        this.displayNotes();
         this.closeForm()
     }
-    diplayNotes(){
+    editNote() {
+        const title = this.$modalTitle.value;
+        const text = this.$modalText.value;
+        this.notes = this.notes.map(note =>
+            note.id === Number(this.editId) ? { ...note, title, text } : note
+        );
+        this.displayNotes();
+    }
+    selectNote(event) {
+        const $selectedNote = event.target.closest('.note');
+        if (!$selectedNote) return;
+        const [$noteTitle, $noteText] = $selectedNote.children;
+        this.editTitle = $noteTitle.innerText;
+        this.editText = $noteText.innerText;
+        this.editId = $selectedNote.dataset.id;
+    }
+    displayNotes(){
         const hasNotes = this.notes.length > 0;
         this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
         this.$note.innerHTML = this.notes.map(note =>
-            `<div  style="background: ${note.color};" class="note">
+            `<div  style="background: ${note.color};" class="note" data-id="${note.id}">
             <div class="${note.title && 'note-title'}">${note.title}</div>
             <div class="note-text">${note.text}</div>
             <div class="toolbar-container">
@@ -76,7 +127,7 @@ class App {
             </div>
         </div>`).join("");
     }
-                    // <img class="toolbar-color" src="https://icon.now.sh/palette">
+
 
 }
 
